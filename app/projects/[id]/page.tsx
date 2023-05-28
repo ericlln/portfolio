5cf5df8b4ce3f221e3ae7bdf6920d857569c4013
Eric Lin <es3lin@uwaterloo.ca>
2023-05-28 12:45:04 -0400

    Fix passing children as prop error

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -33,7 +33,7 @@ export default async function Project({ params }: { params: { id: string } }) {
 					<div className=" m-auto mt-12">
 						<h1 className="text-highlight text-2xl font-semibold mb-4">{blog!.title}</h1>
 						<div className="prose lg:prose-md mb-12">
-							<ReactMarkdown children={blog!.markdown!} remarkPlugins={[remarkGfm]} />
+							<ReactMarkdown remarkPlugins={[remarkGfm]}>{blog!.markdown!}</ReactMarkdown>
 						</div>
 					</div>
 				</div>
