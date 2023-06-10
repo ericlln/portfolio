06f95ab643875e0c6cb71bfcfe6f2a8bd1eebc05
Eric Lin <es3lin@uwaterloo.ca>
2023-06-10 17:43:05 -0400

    Fix missing key prop on iterator

--- app/projects/loading.tsx
+++ app/projects/loading.tsx
@@ -10,12 +10,12 @@ export default function LoadingProjects() {
 			<Navbar />
 			<div className="flex flex-col gap-x-14 gap-y-8 justify-items-center items-center mx-auto my-12">
 				{placeholderProjects.map((i) => (
-					<div className="flex flex-col bg-gray-100 w-2/3 rounded-lg py-8 px-10 xl:flex-row xl:space-x-10">
+					<div key={i} className="flex flex-col bg-gray-100 w-2/3 rounded-lg py-8 px-10 xl:flex-row xl:space-x-10">
 						<div className="flex flex-col basis-2/3">
-							<TextPlaceholder key={i} />
+							<TextPlaceholder />
 						</div>
 						<div className="basis-1/3 order-first mb-6 xl:mb-0 xl:order-last">
-							<ImagePlaceholder key={i} />
+							<ImagePlaceholder />
 						</div>
 					</div>
 				))}
