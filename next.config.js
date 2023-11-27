c239e0f64cc240a2e7d0b59c1fdc9d01b008455c
Eric Lin <es3lin@uwaterloo.ca>
2023-11-26 21:24:41 -0500

    Remove ibb.co from allowed image hosts

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'i.ibb.co',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
                 hostname: 'cdn.erii.cc',
                 port: '',
                 pathname: '/**',
