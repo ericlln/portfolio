328eef750a844cd9df781be431df648faedaa41e
Eric Lin <es3lin@uwaterloo.ca>
2023-11-30 22:46:35 -0500

    Redirect user to most recent gallery with photos

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,23 @@
 import { redirect } from 'next/navigation';
+import prisma from '@/lib/prisma';
+
+export const revalidate = 0;
+
+async function findMostRecentRecord() {
+    const mostRecentRecord = await prisma.gallery.findMany({
+        take: 1,
+        orderBy: {
+            dateAdded: 'desc',
+        },
+    });
+    return mostRecentRecord;
+}
 
 export default async function Photos() {
-    const today = new Date();
-    const month = today.getMonth() + 1;
-    let monthYear = month.toString() + today.getFullYear();
+    const mostRecentRecord = await findMostRecentRecord();
+    const month = mostRecentRecord[0].dateAdded.getMonth() + 1;
+    const year = mostRecentRecord[0].dateAdded.getFullYear();
+    let monthYear = month.toString() + year;
     monthYear = monthYear.padStart(6, '0');
     redirect(`/gallery/${monthYear}`);
 }
