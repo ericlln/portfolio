c4bb29e5f1cabf0923766ea6a4a44f0b481be100
Eric Lin <es3lin@uwaterloo.ca>
2024-01-14 18:57:40 -0500

    Update personal descriptions

--- src/Home/Heading.tsx
+++ src/Home/Heading.tsx
@@ -10,15 +10,15 @@ const Heading = () => {
                     Eric Lin
                 </h1>
                 <h2 className="mb-4 text-center font-sans text-lg font-semibold text-highlight sm:text-left">
-                    Systems Design Engineering @ UW
+                    2A Systems Design Engineering @ UW
                 </h2>
                 <div className="mb-8 flex justify-center sm:justify-start">
                     <Socials />
                 </div>
                 <p className="w-max-w text-center leading-8 md:w-1/2 md:text-left lg:w-1/3 xl:w-1/4">
-                    Hi, I am currently a first year student at University of Waterloo! I am
-                    interested in all aspects of software development, especially after my recent
-                    internship where I worked in full-stack development. You can read more about my
+                    Hi, I am a second year student at University of Waterloo! I am interested in all
+                    aspects of design and software development, especially after my recent
+                    internships where I worked in full-stack development. You can read more about my
                     experiences in my{' '}
                     <Link href="/projects" className="text-green-800 underline">
                         projects
